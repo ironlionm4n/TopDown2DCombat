393fedaf42f67ecf9b1c4270657c832efaf35679
ironlionm4n <ironlionm4n@gmail.com>
2023-05-26 09:26:49 -0400

    active weapon flips with sprite, slight rotation on Z for sword

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,10 +18,12 @@ namespace PlayerScripts
         public delegate void PlayerEvent(float moveX, float moveY);
         public delegate void PlayerAttackEvent(InputAction.CallbackContext context);
         public delegate void MouseMoveEvent(bool flipX);
+        public delegate void MouseMoveEventWithDirection(bool flipX, float moveX, float moveY);
 
 
         public static event PlayerEvent OnPlayerMoveEvent;
         public static event MouseMoveEvent OnMouseMoveEvent;
+        public static event MouseMoveEventWithDirection OnMouseMoveEventWithDirection;
         public static event PlayerAttackEvent OnPlayerAttackEvent;
         
         private void Awake()
@@ -59,7 +61,9 @@ namespace PlayerScripts
         {
             var mousePosition = _playerControls.MousePosition.MousePosition.ReadValue<Vector2>();
             var playerScreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-            OnMouseMoveEvent?.Invoke(mousePosition.x < playerScreenPoint.x);
+            var shouldFlip = mousePosition.x < playerScreenPoint.x;
+            OnMouseMoveEvent?.Invoke(shouldFlip);
+            OnMouseMoveEventWithDirection?.Invoke(shouldFlip, mousePosition.x, mousePosition.y);
         }
         
         private void MovePlayer()
--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -9,20 +9,37 @@ namespace Weapons
     {
         private Animator _swordAnimator;
         private static readonly int Attack = Animator.StringToHash("Attack");
+        private ActiveWeapon _activeWeapon;
 
         private void Awake()
         {
             _swordAnimator = GetComponent<Animator>();
+            _activeWeapon = GetComponentInParent<ActiveWeapon>();
+            
         }
 
         private void OnEnable()
         {
             PlayerController.OnPlayerAttackEvent += HandlePlayerAttack;
+            PlayerController.OnMouseMoveEventWithDirection += MouseFollowWithOffset;
         }
 
         private void HandlePlayerAttack(InputAction.CallbackContext context)
         {
             _swordAnimator.SetTrigger(Attack);
+        }
+
+        private void MouseFollowWithOffset(bool shouldFlip, float moveX, float moveY)
+        {
+            var angle = Mathf.Atan2(moveY, moveX) * Mathf.Rad2Deg;
+            if (shouldFlip)
+            {
+                _activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            }
+            else
+            {
+                _activeWeapon.transform.rotation = Quaternion.Euler(0, 0 ,angle);
+            }
         }
     }
 }
